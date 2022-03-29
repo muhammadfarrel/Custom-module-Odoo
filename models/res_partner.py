97f7a98e1eb4a24a7e16d1f6b4217d262f8898f5
mfarrel <muhammad.farrel@hashmicro.com>
2022-03-29 12:29:06 +0800

    edit customer

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,5 +4,4 @@ from odoo import api, fields,models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    # is_pegawainya = fields.Boolean(string='Pegawai', default=False)
     is_customernya = fields.Boolean(string='Customer', default=False)
